bf5a866a83de2fd053248505a0be068d40a573bf
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-06-03 12:25:36 +0200

    Add product images

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,4 +7,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       new URL('https://pub-f43930670c1848d38f9e9242a40ca7e2.r2.dev/**'),
-      new URL('https://images.unsp
+      new URL('https://images.unsplash.com/**'),
+    ],
+  },
+};
+
+export default nextConfig;
